453bd62177878f9c3221e7003ad60e23a37ca2cf
wangyang <1749748955@qq.com>
2026-02-13 10:24:12 +0800

    update

--- main.go
+++ main.go
@@ -1,11 +1,10 @@
 package main
 
 import (
-	hi "github.com/golangllm/hello/service-hi"
 	say "github.com/golangllm/hello/service-say"
 )
 
 func main() {
 	say.Say()
-	hi.Hello()
+	// hi.Hello()
 }
---
+++ service-say/go.mod
@@ -0,0 +1,3 @@
+module github.com/golangllm/service-say
+
+go 1.25.6
